d45c2f18bfcb6fdb4a64ec3fc3277dc7630cde68
Volleymord <dori@kdwelt.de>
2020-05-30 12:08:30 +0200

    Added comments

--- Ue05/voltage_extract.sh
+++ Ue05/voltage_extract.sh
@@ -18,16 +18,22 @@ if [[ $# != 0 ]]; then
 fi
 
 files=`ls syslog*`
+# check if there are files maching syslog*
 if [[ -z "$files" ]]; then
 	>&2 echo ${ERRMSG[$ERR_NO_FILES]}
 	exit $ERR_NO_FILES
 fi
 
+# explain the columns
 echo -e "file\t\tmax\tmin"
+# go through the files one by one
 for file in $files; do
+	# extract voltages, sort them numerically
 	volts=`cat "$file" | awk '{ print $3 }' | sort -n`
+	# extract max and min value (in first and last line after sorting)
 	min=`echo "$volts" | head -n1`
 	max=`echo "$volts" | tail -n1`
+	# print the result
 	echo -e "${file}\t${max}\t${min}"
 done
 
